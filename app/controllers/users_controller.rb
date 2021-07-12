4ff4df9d4f5d9f8cf994cdc94ea5e17eb8561de7
Rintaro731 <matsurin-0605-blue@au.com>
2021-07-12 21:31:54 +0000

    users[show][edit]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,49 @@
 class UsersController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
+    @post = Post.new
+    @posts = Post.where(user_id: @user.id)
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to user_puth(current_user)
+    end
   end
 
   def update
+     @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
   end
 
   def follows
+    user = User.find(params[:id])
+    @user = user.followings
   end
 
   def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 
 end
